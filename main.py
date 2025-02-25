9c31718113425cb6d8f89bccbee93f4977756515
Iancso Laurentiu-Ioan <iancso.laurentiu.ioan@gmail.com>
2025-02-25 13:53:01 +0200

    Remove dotenv import

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from os import getenv
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Optional: add contact me email functionality (Day 60)
 from smtplib import SMTP
